21fd8fcf99fca0ddfce4d12bd4485aa44e2ef6d4
YevgenyPups <ydobrinets@gmail.com>
2026-03-24 20:41:12 +0300

    add password validator and its tests

---
+++ tests/domain/test_password_validator.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src.domain.exceptions import PasswordValidationError
+from src.domain.password_validator import PasswordValidator
+
+
+@pytest.mark.parametrize(
+    "password",
+    [
+        "12345678",
+        "a" * 128,
+        "password123",
+        "my_secret",
+    ],
+)
+def test_valid_passwords(password):
+    validator = PasswordValidator()
+    validator(password)
+
+
+@pytest.mark.parametrize(
+    "password",
+    [
+        "",  # empty
+        "1234567",  # too short
+        "a" * 129,  # too long
+    ],
+)
+def test_invalid_passwords(password):
+    validator = PasswordValidator()
+    with pytest.raises(PasswordValidationError):
+        validator(password)
