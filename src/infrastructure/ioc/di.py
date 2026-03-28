034e81ed9648235fb8a6b939f02cb07eefc91b01
YevgenyPups <ydobrinets@gmail.com>
2026-03-28 12:27:42 +0300

    add new providers for dishka

--- src/infrastructure/ioc/di.py
+++ src/infrastructure/ioc/di.py
@@ -1,9 +1,25 @@
 from dishka import Provider
 
-from .providers import DBSessionProvider, SettingsProvider
+from .providers import (
+    DBSessionProvider,
+    HelperProvider,
+    MapperProvider,
+    RepoProvider,
+    SettingsProvider,
+    UOWProvider,
+    UseCaseProvider,
+)
 
 
 def get_providers() -> dict[str, Provider]:
     """Return all DI providers grouped by domain."""
 
-    return {"settings": SettingsProvider(), "db": DBSessionProvider()}
+    return {
+        "settings": SettingsProvider(),
+        "db": DBSessionProvider(),
+        "mappers": MapperProvider(),
+        "repos": RepoProvider(),
+        "uow": UOWProvider(),
+        "use_cases": UseCaseProvider(),
+        "helpers": HelperProvider(),
+    }
--- src/infrastructure/ioc/providers.py
+++ src/infrastructure/ioc/providers.py
@@ -8,7 +8,17 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
+from src.application.use_cases import RegisterUser
+from src.domain.interfaces.password_hasher import IPasswordHasher
+from src.domain.interfaces.uow import IUnitOfWork
+from src.domain.interfaces.user_repo import IUserRepo
+from src.domain.password_validator import PasswordValidator
+from src.infrastructure.auth.password_hasher import PasswordHasher
+from src.infrastructure.db.uow import UnitOfWork
+
 from ..config.db import DBSettings
+from ..db.mappers import UserMapper
+from ..db.repos import UserRepo
 
 
 class SettingsProvider(Provider):
@@ -51,3 +61,64 @@ class DBSessionProvider(Provider):
 
         async with session_factory() as session:
             yield session
+
+
+class MapperProvider(Provider):
+    """Provider for domain mappers."""
+
+    @provide(scope=Scope.APP)
+    def get_user_mapper(self) -> UserMapper:
+        """Provide user mapper."""
+
+        return UserMapper()
+
+
+class RepoProvider(Provider):
+    """Provider for repository instances."""
+
+    @provide(scope=Scope.REQUEST)
+    def get_user_repo(session: AsyncSession, mapper: UserMapper) -> IUserRepo:
+        """Provides a UserRepository instance."""
+
+        return UserRepo(session, mapper)
+
+
+class UOWProvider(Provider):
+    """Provider for Unit of Work instances."""
+
+    @provide(scope=Scope.REQUEST)
+    def get_uow(session: AsyncSession, user_repo: IUserRepo) -> IUnitOfWork:
+        """Provides a UnitOfWork instance."""
+
+        return UnitOfWork(session, user_repo)
+
+
+class HelperProvider(Provider):
+    """Provider for helper instances."""
+
+    @provide(scope=Scope.APP)
+    def get_password_hasher(self) -> IPasswordHasher:
+        """Provides a PasswordHasher instance."""
+
+        return PasswordHasher
+
+    @provide(scope=Scope.APP)
+    def get_password_validator(self) -> PasswordValidator:
+        """Provides a PasswordValidator instance."""
+
+        return PasswordValidator()
+
+
+class UseCaseProvider(Provider):
+    """Provider for use cases instances."""
+
+    @provide(scope=Scope.APP)
+    def get_register_user(
+        self,
+        uow: IUnitOfWork,
+        password_validator: PasswordValidator,
+        password_hasher: IPasswordHasher,
+    ) -> RegisterUser:
+        """Provides a RegisterUser instance."""
+
+        return RegisterUser(uow, password_validator, password_hasher)
